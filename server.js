0d7536f680d69c22a384aa4d3bfbe60d21f2cda6
Bogdan Niedzwiecki <bogdan.niedzwiecki@possible.com>
2022-02-01 12:51:25 +0100

    file metadata

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var bodyParser = require("body-parser")
 var app = express();
+var multer = require('multer');
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', multer().single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
